c554dfdc170a530c9eaa32c22f99c936ffd8f021
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2021-12-24 13:38:56 +0100

    abandoning the acpi package in favor of a native implementation

--- awesome/widgets/battery.lua
+++ awesome/widgets/battery.lua
@@ -1,35 +1,53 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
+local DIR = "/sys/class/power_supply/BAT0/"
+
 function icon_markup(percentage, color)
 	if color == nil then color = "#687980" end
-	if percentage == nil then percentage = "0" end
-
 	markup = "<span foreground='"..color.."'> </span>"..percentage.."%"
 	return markup
 end
 
+-- use energy_full_design to make tooltip for battery quality
+
 battery =
     awful.widget.watch(
-    "acpi",
+    "cat "..DIR.."present",
     1,
     function(widget, stdout)
 	widget.font = beautiful.font
-	if percentage ~= "" then
-		percentage = stdout:match("%d+%%"):sub(1, -2)
-	end
 
-	if stdout:match("Charging") ~= nil and tonumber(percentage) <= 95 then
-		widget:set_markup(icon_markup(percentage, "#FFB344"))
-	elseif stdout:match("Charging") ~= nil or stdout:match("Full") ~= nil and tonumber(percentage) > 95 then
-		widget:set_markup(icon_markup(percentage, "#6ECB63"))
-	elseif stdout:match("Discharging") ~= nil and tonumber(percentage) <= 30 then
-		widget:set_markup(icon_markup(percentage, "#FF4848"))
+	if stdout:match("1") then
+		local energy_now_file = io.open(DIR.."energy_now", "r")
+		local energy_full_file = io.open(DIR.."energy_full", "r")
+		local status_file = io.open(DIR.."status", "r")
+
+		local energy_now = energy_now_file:read("a")
+		local energy_full = energy_full_file:read("a")
+		local status = status_file:read("a")
+
+		local formula = (tonumber(energy_now) / tonumber(energy_full)) * 100
+		local percentage = math.floor(formula)
+		
+		if status:match("Charging") and percentage <= 95 then
+			widget:set_markup(icon_markup(percentage, "#FFB344"))
+		elseif percentage > 95 then
+			widget:set_markup(icon_markup(percentage, "#6ECB63"))
+		elseif percentage < 30 then
+			widget:set_markup(icon_markup(percentage, "#FF4848"))
+		else
+			widget:set_markup(icon_markup(percentage))
+		end
+
+		-- cleanup
+		energy_now_file:close()
+		energy_full_file:close()
+		status_file:close()
 	else
-		widget:set_markup(icon_markup(percentage) or 0)
+		widget:set_markup("NO BATTERY")
 	end
-
-    end
+end
 )
 
 return battery
