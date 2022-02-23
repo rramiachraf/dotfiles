26db0020e51e3f4066b67bbc9de39e88ce72f99d
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2022-02-23 14:05:44 +0100

    make occupied taglists visible

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -4,16 +4,17 @@ local dpi = xresources.apply_dpi
 
 local theme = {}
 
+-- Palette
 theme.bg = "#161616"
 theme.fg = "#F3F1F5"
 
+-- General
 theme.font          = "jetbrains mono nerd font medium 10"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#aaaaaa"
 theme.fg_focus      = "#ffffff"
@@ -26,12 +27,12 @@ theme.border_normal = "#171717"
 theme.border_focus  = "#112031"
 theme.border_marked = "#91231c"
 
+-- Navbar
 theme.wibar_bg      = theme.bg
 theme.wibar_fg      = theme.fg
 
+-- Systray
 theme.bg_systray    = theme.bg
-
-theme.taglist_bg_focus = "#112031"
 
 -- Tooltip
 theme.tooltip_bg = theme.bg
@@ -39,6 +40,11 @@ theme.tooltip_fg = theme.fg
 theme.tooltip_border_color = theme.fg
 theme.tooltip_border_width = 1
 
+-- Taglist
+theme.taglist_fg_occupied = "#94B49F"
+theme.taglist_bg_focus = "#112031"
+
+-- Wallpaper
 theme.wallpaper = os.getenv("HOME")..".config/awesome/wallpaper.jpg"
 
 -- Define the icon theme for application icons. If not set then the icons
