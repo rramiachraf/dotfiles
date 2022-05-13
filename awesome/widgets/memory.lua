dd2f13c4f91c36ed1ce1458cf25864faef073327
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2022-05-13 16:55:38 +0100

    add total to memory widget

--- awesome/widgets/memory.lua
+++ awesome/widgets/memory.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
-local cmd = [[bash -c "echo `free -h --mega | awk '{if(NR==2) print $3}'`"]]
+local cmd = [[bash -c "echo `free -h --mega | awk '{if(NR==2) print $3\"/\"$2}'`"]]
 
 memory_widget =
     awful.widget.watch(
